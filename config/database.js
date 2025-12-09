7ee67dde75f4e39d82b950d466913c51a5d26b4b
Codyaxe <vincelorzano@gmail.com>
2025-12-09 20:33:59 +0800

    build: database.js modification

--- config/database.js
+++ config/database.js
@@ -1,16 +1,24 @@
-const Sequelize = require('sequelize');
-const env = require('dotenv');
+const Sequelize = require("sequelize");
+const env = require("dotenv");
 env.config();
 
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT || 3306;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
-const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  dialect: 'mysql',
-  host: DB_HOST
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  dialect: "mysql",
+  host: DB_HOST,
+  port: DB_PORT,
+  logging: false, // Disable SQL logging in production
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 module.exports = sequelize;
